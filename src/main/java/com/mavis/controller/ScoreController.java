0c6aafeef450222abe67aa0c04bb455b3c13799c
郭逸轩 <mcymzc@qq.com>
2024-05-06 15:58:02 +0800

    初始化项目模型

--- src/main/java/com/mavis/controller/ScoreController.java
+++ src/main/java/com/mavis/controller/ScoreController.java
@@ -1,12 +1,16 @@
 package com.mavis.controller;
 
+import com.mavis.entity.Score;
 import com.mavis.logic.ScoreLogic;
 import com.mavis.service.ScoreService;
+import com.mavis.utils.RestResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * ScoreController
@@ -22,4 +26,14 @@ public class ScoreController {
     private ScoreService scoreService;
     @Resource
     private ScoreLogic scoreLogic;
+
+    /**
+     * 获取所有成绩
+     * @return
+     */
+    @GetMapping("getallscore")
+    public RestResult getAllScore(){
+        List<Score> scores = scoreService.list();
+        return RestResult.success(scores);
+    }
 }
